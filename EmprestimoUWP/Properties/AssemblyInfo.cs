76c799469576b5bdaf289a4741ccfab947f0282d
laviniameds <laviniameds@icloud.com>
2016-11-28 17:29:23 -0300

    interface em desenvolvimento.

--- EmprestimoUWP/Properties/AssemblyInfo.cs
+++ EmprestimoUWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EmprestimoUWP.WindowsPhone")]
+[assembly: AssemblyTitle("EmprestimoUWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EmprestimoUWP.WindowsPhone")]
+[assembly: AssemblyProduct("EmprestimoUWP")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
